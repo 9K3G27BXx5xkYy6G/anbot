6cdc0703c34d00abab1b2597856e5f42aa645d56
Kyle Taylor <ktaylora@users.noreply.github.com>
2023-01-31 13:49:20 -0700

    Update chat.py

--- anbot/chat.py
+++ anbot/chat.py
@@ -1,11 +1,103 @@
+import emoji
+import logging
+
 from matrix_bot_api.matrix_bot_api import MatrixBotAPI
 from matrix_bot_api.mregex_handler import MRegexHandler
 from matrix_bot_api.mcommand_handler import MCommandHandler
 from matrix_client.api import quote # for shims
 
-import emoji
+logger = logging.getLogger(__name__)
 
-import services
+
+##### The interactions with chat services are separated from bot behavior.
+##### This lets one change what platform the bot operates on, or what library it uses,
+#####  by only changing a small part of the code.
+##### Separating things this way is important when writing software.
+
+class Event:
+    def __init__(self, service, room, id, sender, type, data=None, raw=None, reply=None):
+        self.service = service
+        self.room = room
+        self.id = id
+        self.sender = sender
+        self.type = type
+        self.data = data
+        self.raw = raw
+        self.reply = reply
+
+class Room:
+    def __init__(self, service, name, voice, members=[], history=[], raw=None):
+        self.service = service
+        self.name = name
+        self.voice = voice
+        self.members = members
+        self.history = history
+        self.raw = raw
+    def send(self, message):
+        return self.service.send(self, message)
+
+class Services:
+    def __init__(self):
+        self.services = []
+    def wait(self):
+        for service in self.services:
+            service.wait()
+    def add(self, service):
+        self.services.append(service)
+        service.start()
+    def stop(self):
+        for service in self.services:
+            service.stop()
+    def add_matrix(self, username, password, server):
+        import service_matrix
+        self.add(service_matrix.Matrix(self, username, password, server))
+
+    def on_member(self, event):
+        self.on_message(event)
+    def on_other(self, event):
+        self.on_message(event)
+    def on_message(self, event):
+        logger.debug(f'{event.service} {event.room} {event.id} {event.sender} {event.type} {event.data} reply={event.reply}')
+        pass
+
+    def _on_error(self, event, exception):
+        import traceback
+        exc_str = traceback.format_exc()
+        logger.error(f'{event.service} {event.room.name} {event.id} {event.sender} {event.type} {event.data} reply={event.reply}')
+        for line in exc_str.split('\n'):
+            logger.error(line)
+        self.on_error(event, exception, exc_str)
+    def _on_event(self, event):
+        try:
+            self.log(event.service, event.room, event.sender, event.data or '<no data>')
+            if type == 'message':
+                if event.sender != service.user_id:
+                    self.on_message(event)
+            elif type == 'membership':
+                self.on_membership(event)
+            else:
+                self.on_other(event)
+        except Exception as exception:
+            self._on_error(event=event, exception=exception)
+
+    def log(self, service, room, sender, text):
+        log_lines = text.split('\n')
+        padding = ' ' * len(sender)
+        logger.info(f'{room.name} {sender}: {log_lines[0]}')
+        for extra_line in log_lines[1:]:
+            logger.info(f'{room.name} {padding}: {extra_line}')
+
+class TempMessage:
+    def __init__(self, room, method, *params, **kwparams):
+        self.room = room
+        self.method = method
+        self.params = params
+    def __enter__(self):
+        self.msg = self.method(self.room, *self.params, **self.kwparams)
+        return self.msg
+    def __exit__(self, *params):
+        self.room.service.delete(self.room, self.msg)
+        self.msg = None
 
 class MatrixRoom(services.Room):
     def __init__(self, service, room):
