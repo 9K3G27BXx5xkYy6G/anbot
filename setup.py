6d37828d84ed1a71210b785603b737cbe52c66de
Kyle Taylor <kyle.taylor@intent.ag>
2023-01-31 12:09:35 -0700

    initial push

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="anbot",
+    version="0.0.1",
+    description="A chatbot that uses smart caching to interact with upstream API's, such as ChatGPT and GPT-3",
+    author="",
+    copyright="2023",
+    license="GPLv3",
+    maintainer="xloem",
+    maintainer_email="",
+    url="https://github.com/9K3G27BXx5xkYy6G/anbot",
+    packages=["anbot"],
+)
