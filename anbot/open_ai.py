5aef320bfcf79a9591fc7ed99d47cade7e2b5f0b
xloem <0xloem@gmail.com>
2023-02-01 22:52:48 -0500

    obfuscate key

--- anbot/open_ai.py
+++ anbot/open_ai.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
-    import os
-    os.environ.setdefault('OPENAI_API_KEY', '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')
+    import base64, os
+    os.environ.setdefault('OPENAI_API_KEY', base64.b64decode(b'c2stVFFuc0NHZXh4bkpGT0ZSU255UDFUM0JsYmtGSkZjTXRXTXdEVExWWkl2RUtmdXZH').decode())
     from langchain.llms import OpenAI
     openai = OpenAI()
     response = openai.generate(['Song lyrics about burning dollar bills in the style of Chumbawumba.'])
