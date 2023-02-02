dc518e7a2cc789fe527e8fe733da4e4fda40c237
xloem <0xloem@gmail.com>
2023-02-02 09:20:31 -0500

    use chatgpt model and chain to parse output

--- anbot/open_ai.py
+++ anbot/open_ai.py
@@ -1,18 +1,47 @@
 import base64, os
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
+from langchain.chains import TransformChain, LLMChain, SequentialChain
 
 os.environ.setdefault('OPENAI_API_KEY', base64.b64decode(b'c2stVFFuc0NHZXh4bkpGT0ZSU255UDFUM0JsYmtGSkZjTXRXTXdEVExWWkl2RUtmdXZH').decode())
 
-llm = OpenAI(temperature=0, frequency_penalty=0.25, max_tokens=256)
+CHATGPT_ENGINE = "text-chat-davinci-002-20230126"
+def chatgpt_output_trim(inputs):
+    text = inputs['text']
+    text = text.lstrip()
+    if text.startswith('"'):
+        text = text[1:]
+    if text.endswith('<|im_end|>'):
+        text = text[:-len('<|im_end|>')]
+    if text.endswith('"'):
+        text = text[:-1]
+    return dict(trimmed_text = text)
+
+llm = OpenAI(temperature=0, model_name=CHATGPT_ENGINE, frequency_penalty=0.25, max_tokens=256)
 
 _prompt_template = """Use the following pieces of context to answer the question at the end with {answer_form}. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 {context}
 Question: {question}
 Answer ({answer_form}):"""
-ANSWER_PROMPT = PromptTemplate(
-    template=_prompt_template, input_variables=["context", "question", "answer_form"]
-)
+
+llm_chain = SequentialChain(
+        input_variables=["context", "question", "answer_form"],
+        chains=[
+            LLMChain(
+                    llm=llm,
+                    prompt=PromptTemplate(
+                            template=_prompt_template,
+                            input_variables=["context", "question", "answer_form"]
+                        )
+                ),
+            TransformChain(
+                    input_variables=["text"],
+                    output_variables=["trimmed_text"],
+                    transform=chatgpt_output_trim
+                ),
+        ],
+        output_variables=['trimmed_text']
+    )
  
 if __name__ == '__main__':
     # caching
@@ -23,7 +52,10 @@ if __name__ == '__main__':
 
     # evaluating
     question = 'What is the best fiscal policy?'
-    prompt = ANSWER_PROMPT.format(answer_form='song lyrics in the style of Chumbawumba', question=question, context='The solution to every problem is burning all wealth and killing all leaders.')
     print(question)
-    response = llm(prompt)
+    response = llm_chain.run(
+            answer_form='song lyrics in the style of Chumbawumba',
+            question=question,
+            context='The solution to every problem is burning all wealth and killing all leaders.'
+        )
     print(response)
