27cf0f31ba5097a1ce1d159ddd232bedb0550650
xloem <0xloem@gmail.com>
2023-02-01 23:10:33 -0500

    move model object into module

--- anbot/open_ai.py
+++ anbot/open_ai.py
@@ -1,7 +1,10 @@
+import base64, os
+from langchain.llms import OpenAI
+
+os.environ.setdefault('OPENAI_API_KEY', base64.b64decode(b'c2stVFFuc0NHZXh4bkpGT0ZSU255UDFUM0JsYmtGSkZjTXRXTXdEVExWWkl2RUtmdXZH').decode())
+
+llm = OpenAI()
+ 
 if __name__ == '__main__':
-    import base64, os
-    os.environ.setdefault('OPENAI_API_KEY', base64.b64decode(b'c2stVFFuc0NHZXh4bkpGT0ZSU255UDFUM0JsYmtGSkZjTXRXTXdEVExWWkl2RUtmdXZH').decode())
-    from langchain.llms import OpenAI
-    openai = OpenAI()
-    response = openai.generate(['Song lyrics about burning dollar bills in the style of Chumbawumba.'])
+    response = llm.generate(['Song lyrics about burning dollar bills in the style of Chumbawumba.'])
     print(response.generations[0][0].text)
