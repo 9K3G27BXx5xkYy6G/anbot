9967966065591ef5d22a1fd0b1e2c0b5c037283c
Kyle Taylor <kyle.a.taylor@gmail.com>
2023-02-05 19:26:28 -0700

    initial implementation for RSS/ATOM feed backends for reddit and anews

---
+++ anbot/tasks.py
@@ -0,0 +1,19 @@
+import hashlib
+from typing import Dict, List, Any
+
+sha1 = lambda x: hashlib.sha1(str(x).encode("utf-8")).hexdigest()
+
+
+def cache_reddit_top_post(hash: bool = True, timeframe: str = "weekly") -> Dict[str]:
+    top_post = {}
+    try:
+        top_post = RedditBackend(timeframe=timeframe)
+        if top_post.content:
+            top_post = top_post_weekly.content[0]
+        else:
+            top_post = {}
+    except Exception:
+        pass
+    if hash:
+        top_post["hash"] = sha1(top_post["title"])
+    return top_post
