61db1ce1bd08e048103485ffca1907ebc27939eb
Kyle Taylor <kyle.a.taylor@gmail.com>
2023-02-05 19:04:52 -0700

    initial implementation for RSS/ATOM feed backends for reddit and anews

--- anbot/anews.py
+++ anbot/anews.py
@@ -6,34 +6,30 @@ except ImportError:
     pass
 
 
-class Reddit:
+class ANews:
     def __init__(self, **kwargs):
         """ """
-        self.subreddit = kwargs.get("subreddit", "anarchism")
-        self.interval = kwargs.get("interval", "week")
         self.content = []
         self.rss = None
-        self._subreddit_atom_feed = (
-            f"https://www.reddit.com/r/{self.subreddit}/top.rss?t={self.interval}"
-        )
+        self._anews_rss_feed = "https://anarchistnews.org/rss.xml"
         self.headers = kwargs.get(
             "headers",
             {
                 "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
             },
         )
-        if self._subreddit_atom_feed:
+        if self._anews_rss_feed:
             self.get()
         if self.rss:
             self.parse()
 
     def get(self) -> None:
-        """Wrapper for requests GET method that can forge a convincing header for requests of a sub-reddit's ATOM feed url"""
+        """Wrapper for requests GET method that can forge a convincing header for requests of a RSS feed url"""
         try:
-            self.rss = requests.get(self._subreddit_atom_feed, headers=self.headers)
+            self.rss = requests.get(self._anews_rss_feed, headers=self.headers)
             if self.rss.status_code != 200:
                 raise RuntimeError(
-                    f"http GET request failed for url: {self._subreddit_atom_feed}"
+                    f"http GET request failed for url: {self._anews_rss_feed}"
                 )
             self.rss = self.rss.content
             self.rss = BeautifulSoup(self.rss, "xml")
@@ -41,16 +37,16 @@ class Reddit:
             raise e
 
     def parse(self) -> None:
-        """Parses an ATOM feed into a list of dictionaries with 'link' and 'title' key/value pairs"""
+        """Parses an RSS feed into a list of dictionaries with 'link' and 'title' key/value pairs"""
         if self.rss:
             try:
                 self.content = [
-                    BeautifulSoup(c.text, "html") for c in self.rss.find_all("content")
+                    BeautifulSoup(c.text, "html") for c in self.rss.find_all("item")
                 ]
                 self.content = [
-                    {"link": c.find("a")["href"], "title": c.find("img")["title"]}
+                    {"link": c.find("link"), "title": c.find("title")}
                     for c in self.content
-                    if c.find("img") and c.find("a")
+                    if c.find("link") and c.find("title")
                 ]
             except Exception as e:
                 raise e
